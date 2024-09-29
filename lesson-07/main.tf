0bc6efa590670572e6f0ad0d7d3891307e924f71
havingchan <61160874@qq.com>
2024-09-29 11:38:09 +0800

    completed lesson-07

--- lesson-07/main.tf
+++ lesson-07/main.tf
@@ -2,21 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.20"
+      version = "~> 5.56"
     }
   }
 
-  required_version = ">= 1.2.8"
+  required_version = ">= 1.7.3"
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region     = "us-east-2"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 }
 
 resource "aws_instance" "lesson_07" {
   ami           = "ami-0c7c4e3c6b4941f0f"
   instance_type = "t2.micro"
   tags = {
-    Name      = "Lesson-07-Variables!!!"
+    Name = var.instance_name
   }
 }
---
+++ lesson-07/variables.tf
@@ -0,0 +1,18 @@
+variable "instance_name" {
+  # description = "Name tag of the instance"
+  # type        = string
+  # default     = "Lesson-07-Variables!!!"
+}
+
+variable "AWS_ACCESS_KEY" {
+  description = "AWS Access Key"
+  type        = string
+
+
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "AWS Secret"
+  type        = string
+
+}
